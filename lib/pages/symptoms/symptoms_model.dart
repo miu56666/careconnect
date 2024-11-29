236665ae4fc5531eccc168849e87bb1ef4c9d224
shh <ALbadawi@DESKTOP-1SM3NNC.(none)>
2024-11-29 18:47:13 +0300

    symptoms

---
+++ lib/pages/symptoms/symptoms_model.dart
@@ -0,0 +1,11 @@
+import '/flutter_flow/flutter_flow_util.dart';
+import 'symptoms_widget.dart' show SymptomsWidget;
+import 'package:flutter/material.dart';
+
+class SymptomsModel extends FlutterFlowModel<SymptomsWidget> {
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
---
+++ lib/pages/symptoms/symptoms_widget.dart
@@ -0,0 +1,221 @@
+import '/backend/supabase/supabase.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import 'package:flutter/material.dart';
+import 'symptoms_model.dart';
+export 'symptoms_model.dart';
+
+class SymptomsWidget extends StatefulWidget {
+  const SymptomsWidget({
+    super.key,
+    required this.symptoms,
+  });
+
+  final VaccinesRow? symptoms;
+
+  @override
+  State<SymptomsWidget> createState() => _SymptomsWidgetState();
+}
+
+class _SymptomsWidgetState extends State<SymptomsWidget> {
+  late SymptomsModel _model;
+
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    _model = createModel(context, () => SymptomsModel());
+
+    WidgetsBinding.instance.addPostFrameCallback((_) => safeSetState(() {}));
+  }
+
+  @override
+  void dispose() {
+    _model.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        key: scaffoldKey,
+        body: SafeArea(
+          top: true,
+          child: Container(
+            width: 388.0,
+            height: 810.0,
+            decoration: BoxDecoration(
+              color: FlutterFlowTheme.of(context).secondaryBackground,
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: Image.asset(
+                  'assets/images/5ry6m_.jpg',
+                ).image,
+              ),
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Container(
+                  decoration: const BoxDecoration(
+                    color: Color(0x41B9D8CE),
+                  ),
+                  child: Padding(
+                    padding:
+                        const EdgeInsetsDirectional.fromSTEB(0.0, 15.0, 5.0, 0.0),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsetsDirectional.fromSTEB(
+                              20.0, 0.0, 0.0, 0.0),
+                          child: InkWell(
+                            splashColor: Colors.transparent,
+                            focusColor: Colors.transparent,
+                            hoverColor: Colors.transparent,
+                            highlightColor: Colors.transparent,
+                            onTap: () async {
+                              context.pushNamed('homePage');
+                            },
+                            child: Icon(
+                              Icons.arrow_back_ios_sharp,
+                              color: FlutterFlowTheme.of(context).primaryText,
+                              size: 24.0,
+                            ),
+                          ),
+                        ),
+                        Align(
+                          alignment: const AlignmentDirectional(0.0, -1.0),
+                          child: Padding(
+                            padding: const EdgeInsetsDirectional.fromSTEB(
+                                0.0, 5.0, 10.0, 0.0),
+                            child: Text(
+                              'تتبع الأعراض',
+                              textAlign: TextAlign.center,
+                              style: FlutterFlowTheme.of(context)
+                                  .titleLarge
+                                  .override(
+                                    fontFamily: 'Gulzar',
+                                    color: const Color(0xEE637D87),
+                                    fontSize: 25.0,
+                                    letterSpacing: 0.0,
+                                    fontWeight: FontWeight.normal,
+                                    useGoogleFonts: false,
+                                  ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: const AlignmentDirectional(1.0, 0.0),
+                  child: Padding(
+                    padding:
+                        const EdgeInsetsDirectional.fromSTEB(0.0, 25.0, 20.0, 0.0),
+                    child: Text(
+                      'أعراض ما بعد التطعيم',
+                      textAlign: TextAlign.end,
+                      style: FlutterFlowTheme.of(context).bodyMedium.override(
+                            fontFamily: 'Amiri',
+                            fontSize: 20.0,
+                            letterSpacing: 0.0,
+                            fontWeight: FontWeight.bold,
+                            useGoogleFonts: false,
+                          ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsetsDirectional.fromSTEB(0.0, 15.0, 0.0, 0.0),
+                  child: Container(
+                    width: 367.0,
+                    height: 200.0,
+                    decoration: BoxDecoration(
+                      color: const Color(0x356095C6),
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                    child: Align(
+                      alignment: const AlignmentDirectional(0.0, 0.0),
+                      child: Padding(
+                        padding:
+                            const EdgeInsetsDirectional.fromSTEB(0.0, 5.0, 20.0, 0.0),
+                        child: Text(
+                          valueOrDefault<String>(
+                            widget.symptoms?.sideEffect,
+                            'اعراض',
+                          ),
+                          textAlign: TextAlign.center,
+                          style:
+                              FlutterFlowTheme.of(context).bodyMedium.override(
+                                    fontFamily: 'Amiri',
+                                    fontSize: 20.0,
+                                    letterSpacing: 0.0,
+                                    fontWeight: FontWeight.w500,
+                                    useGoogleFonts: false,
+                                  ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: const AlignmentDirectional(1.0, 0.0),
+                  child: Padding(
+                    padding:
+                        const EdgeInsetsDirectional.fromSTEB(0.0, 25.0, 20.0, 0.0),
+                    child: Text(
+                      'الإجرائات الوقائية',
+                      textAlign: TextAlign.end,
+                      style: FlutterFlowTheme.of(context).bodyMedium.override(
+                            fontFamily: 'Amiri',
+                            fontSize: 20.0,
+                            letterSpacing: 0.0,
+                            fontWeight: FontWeight.bold,
+                            useGoogleFonts: false,
+                          ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsetsDirectional.fromSTEB(0.0, 15.0, 0.0, 0.0),
+                  child: Container(
+                    width: 367.0,
+                    height: 200.0,
+                    decoration: BoxDecoration(
+                      color: const Color(0x356095C6),
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                    child: Align(
+                      alignment: const AlignmentDirectional(0.0, 0.0),
+                      child: Text(
+                        valueOrDefault<String>(
+                          widget.symptoms?.preventiveMeasures,
+                          'اجراءات',
+                        ),
+                        textAlign: TextAlign.center,
+                        style: FlutterFlowTheme.of(context).bodyMedium.override(
+                              fontFamily: 'Amiri',
+                              fontSize: 20.0,
+                              letterSpacing: 0.0,
+                              fontWeight: FontWeight.w500,
+                              useGoogleFonts: false,
+                            ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
